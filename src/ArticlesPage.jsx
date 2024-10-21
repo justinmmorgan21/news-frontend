7865252132553b97592acad00e31eb92be0fc416
justinmmorgan21 <justinmorgan21@gmail.com>
2024-10-20 20:12:15 -0500

    added mapping of articles and search feature

--- src/ArticlesPage.jsx
+++ src/ArticlesPage.jsx
@@ -1,7 +1,34 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 export function ArticlesPage() {
+
+  const [articles, setArticles] = useState([]);
+  const [searchTerms, setSearchTerms] = useState("")
+
+  const articlesIndex = () => {
+    axios.get(`http://localhost:3000/articles.json?search=${searchTerms}`).then(response => {
+      console.log(response.data.articles);
+      setArticles(response.data.articles);
+      console.log(searchTerms)
+    })
+  }
+
+  useEffect(articlesIndex, [])
+
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <input type="text" onChange={event=>setSearchTerms(event.target.value)}/> <button onClick={() => articlesIndex()}>Search</button>
+      {
+        articles.map(article => (
+          <div key={article.id}>
+            <h3>{article.title}</h3>
+            <p>by {article.author}</p> 
+            <p >{article.description}</p>
+          </div>
+        ))
+      }
     </main>
   )
 }
